77e2c31ac55b73717dc71562ee4aa1496881ab5b
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-17 17:10:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -10,6 +10,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   end
 end
 
+add_supply_to_winter_holidays(holiday_supplies, "Balloons")
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
